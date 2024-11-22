5172f20fcd36118a995c9a4764aea9eda69f69e2
mbjawn <0000111521424xsp@al.educacao.sp.gov.br>
2024-11-22 10:01:41 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'quem o jawn ama?',
+    'clique e saberá',
+    'o jawn ama o marcio e merece nota'
+)
+
+criaCartao(
+    'aluuuuura aluuuura',
+    'clique para saber o que o márcio falou...',
+    'vagabundo!!!!'
+)
+
+criaCartao(
+    'to sem criatividade já',
+    'clique para ganhar um beijo',
+    'safado'
+)
+
+criaCartao(
+    'bom dia,marcio!',
+    'como faz para o passarinho dar risada?',
+    'joga ele na parede que ele racha o bico'
+)
